b98a0eff2fa3e4a495c6a1bfe996f2000b96302f
sai-1vasudha <167866643+sai-1vasudha@users.noreply.github.com>
2024-04-24 16:22:17 +0530

    3rd commit Basics1.py

--- basics/Basics1.py
+++ basics/Basics1.py
@@ -3,3 +3,6 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("http://www.rediff.com")
 driver.maximize_window()
+actual_title = driver.title
+print(actual_title)
+driver.close()
